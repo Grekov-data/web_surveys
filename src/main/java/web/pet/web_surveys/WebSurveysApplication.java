33e616b4f8555b43f3a46fef4865bccce8eae152
Andrew Grekov <andrey.dstko@gmail.com>
2024-03-21 08:49:40 +0700

    Files

---
+++ src/main/java/web/pet/web_surveys/WebSurveysApplication.java
@@ -0,0 +1,13 @@
+package web.pet.web_surveys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSurveysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSurveysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/pet/web_surveys/WebSurveysApplicationTests.java
@@ -0,0 +1,13 @@
+package web.pet.web_surveys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSurveysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
